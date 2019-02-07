c36b25837da1ddd5ba963d7dc0f61c6756cab19a
galya <shvecova.g@gmail.com>
2019-02-07 22:01:33 +0700

    add field and select

--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -1,59 +1,107 @@
+        var height, width, bombs;
+$("#start").click(
 
-        $("#start").click(
-
-function(){
-       var radioValue = $("input[name='sizeCode']:checked").val();
-       var height, width;
-       switch(radioValue) {
+    function(){
+        var radioValue = $("input[name='sizeCode']:checked").val();
+        switch(radioValue) {
         case "small":
-        height = 8;
-        width = 8;
-        break;
+            height = 8;
+            width = 8;
+            bombs = 10;
+            break;
         case "medium":
-        height = 16;
-        width = 16;
-        break;
+            height = 16;
+            width = 16;
+            bombs = 40;
+            break;
         case "large":
-        height = 16;
-        width = 30;
-        break;
+            height = 16;
+            width = 30;
+            bombs = 99;
+            break;
         case "custom":
-        height = $("#height").value;
-        width = $("#width").value;
-        break;
+            height = $("#height").value;
+            width = $("#width").value;
+            bombs = $("#bombs").value;
+            break;
        }
-       $.ajax({
-         type: "POST",
-         url: "/start",
-         data: JSON.stringify({
-           fieldHeight : height,
-           fieldWidth : width,
-           bombsCount : $("#bombs").value,
-           playerName : $("#player").value
-           }),
-           success: function(data){
-          $('.menu').hide(400);
-          $('.game').show();
-           },
-         contentType : "application/json"
-       });}
 
+       init();
+
+       $.ajax({
+        type: "POST",
+        url: "/start",
+        data: JSON.stringify({
+            fieldHeight : height,
+            fieldWidth : width,
+            bombsCount : bombs,
+            playerName : $("#player").value
+            }),
+        success: function(data){
+            $('.menu').hide(400);
+            $('.game').show();
+            },
+        contentType : "application/json"
+       });
+       }
     );
 
 
-        $("#back").click(
+$("#back").click(
 
 function(){
-
-       $.ajax({
-           success: function(data){
-        $('.menu').show(400);
-          $('.game').hide();
-           },
-       });}
-
+            $('.menu').show(400);
+            $('.game').hide();}
     );
 
+function init() {
+    var $table = $('<table/>');
+
+    for (var i = 0; i < width; i++) {
+        var $row = $('<tr/>');
+
+        for (var j = 0; j < height; j++) {
+            var $col = $('<td>0<td/>');
+            $col.data({x:i,y:j});
+            $col.addClass("empty");
+            var clickType;
+
+            $col.mousedown(function(event) {
+                switch (event.which) {
+                    case 3:
+                        clickType = 'mark';
+                        break;
+                    default:
+                        clickType = 'open';
+                }
 
 
+
+
+                 $.ajax({
+                                    type: "POST",
+                                    url: "/select",
+                                    data: JSON.stringify($(event.target).data()),
+                                    success: function(data){
+                                            //render(data);
+                                        },
+                                    contentType : "application/json"
+                                   });
+                                   return false;
+            });
+
+
+            $row.append($col);
+        }
+
+        $table.append($row);
+    }
+
+    $('#field').append($table);
+}
+
+
+function onLeftClick() {
+
+}
 
